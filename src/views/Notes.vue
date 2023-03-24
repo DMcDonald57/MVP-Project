c7fada13f0226ac819caddd0ecec3ba197f4d6d2
DMcDonald57 <d.mcdonald579@gmail.com>
2023-03-23 20:28:58 -0600

    final commit before submission.  Rough Fig file added as guidline  Added notes page to guide vision for project.

---
+++ src/views/Notes.vue
@@ -0,0 +1,27 @@
+<!-- Home Page -->
+    <!-- Nav Bar    DONE -->
+    <!-- Message board or Important Dates with add/update etc buttons -->
+    <!-- Map for location of playing fields     DONE -->
+    <!-- Only an executive member may log in and make changes to home page -->
+    <!-- CSS work to improve banner, colors and alignments of containers-->
+
+<!-- league Rules -->
+    <!-- simple page to display league rules -->
+    <!-- only executive membe may log in to update or change -->
+
+<!-- Team page -->
+    <!-- displays current roster without contact info   DONE -->
+    <!-- log in space for coaches to make changes to roster page  DONE -->
+    <!-- team message board or key dates that can be only updated by coach -->
+
+<!-- log in page -->
+    <!-- simple email password for coaches with hidden password input  DONE -->
+    <!-- NO REGISTATION page as I did not anyone to be able to register -->
+    <!-- I believe coaches would be added to DB manually by web page developer -->
+
+<!-- Roster update page -->
+    <!-- displays button to list roster of coach who is logged in with contact
+    info for the team included -->
+    <!-- buttons to add/change or delete roster members -->
+    <!-- log out button that would delete session and return coach back to home
+    page or team page -->
